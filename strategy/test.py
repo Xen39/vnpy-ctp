731b5c9ed61a558f5684f22edb0400e6be8c3dfc
Xen39 <2294148317@qq.com>
2025-04-29 11:43:55 +0800

    add MyTestStrategy

---
+++ strategy/test.py
@@ -0,0 +1,70 @@
+import sys
+
+from vnpy_ctastrategy import *
+from vnpy.trader.constant import Interval
+from ctp.output import to_string
+
+
+class MyTestStrategy(CtaTemplate):
+    """测试策略"""
+
+    author = "Xen39"
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """初始化策略"""
+        super().__init__(cta_engine, strategy_name, vt_symbol, setting)
+
+        # 创建K线合成器
+        self.bg = BarGenerator(on_bar=self.on_bar, window=1, on_window_bar=None, interval=Interval.MINUTE, daily_end=None)
+        # 创建时间序列管理器
+        self.am = ArrayManager(size=5)
+        print(f"{self.strategy_name} __init__()")
+
+    def on_init(self):
+        """策略初始化回调"""
+        print(f"{self.strategy_name} on_init()")
+
+    def on_start(self):
+        """策略启动回调"""
+        print(f"{self.strategy_name} on_start()")
+        self.put_event()
+
+    def on_stop(self):
+        """策略停止回调"""
+        print(f"{self.strategy_name} on_stop()")
+        self.put_event()
+
+    def on_tick(self, tick: TickData):
+        """Tick数据更新回调"""
+        # print(f"{self.strategy_name} on_tick()")
+        self.bg.update_tick(tick)
+
+    def on_bar(self, bar: BarData):
+        """K线数据更新回调"""
+        print(f"{self.strategy_name} on_bar()", to_string(bar))
+        self.am.update_bar(bar)
+        if not self.am.inited:
+            print(f"{self.strategy_name} not inited: count={self.am.count}, size={self.am.size}")
+            return
+
+        if self.pos == 0:
+            self.buy(bar.close_price, 1)
+        elif self.pos == 1:
+            self.sell(bar.close_price, 1)
+        else:
+            print(f"unexpected position: {self.pos}", file=sys.stderr)
+
+        self.put_event()
+
+    def on_order(self, order: OrderData):
+        """委托更新回调"""
+        print(f"{self.strategy_name} on_order()", to_string(order))
+
+    def on_trade(self, trade: TradeData):
+        """成交更新回调"""
+        print(f"{self.strategy_name} on_trade()", to_string(trade))
+        self.put_event()
+
+    def on_stop_order(self, stop_order: StopOrder):
+        """停止单更新回调"""
+        print(f"{self.strategy_name} on_stop_order()", to_string(stop_order))
