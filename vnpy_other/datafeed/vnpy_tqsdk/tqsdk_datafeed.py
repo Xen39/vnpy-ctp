4acb92ae4afe2d483cdd268dcc7927eaab6421e8
Xen39 <2294148317@qq.com>
2025-04-30 15:26:52 +0800

    restored vnpy_tqsdk

--- vnpy_other/datafeed/vnpy_tqsdk/tqsdk_datafeed.py
+++ vnpy_other/datafeed/vnpy_tqsdk/tqsdk_datafeed.py
@@ -38,7 +38,6 @@ class TqsdkDatafeed(BaseDatafeed):
             output(traceback.format_exc())
             return None
 
-        api._auth._grants["features"].append("tq_dl")
         # 查询数据
         interval: str = INTERVAL_VT2TQ.get(req.interval, None)
         if not interval:
