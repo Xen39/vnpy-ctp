fbccc89f43e752ce7ab249a754f69132653ceb26
Xen39 <2294148317@qq.com>
2025-05-07 11:17:50 +0800

    refactored test strategy
    - buy & sell only once
    - fixed log error

--- strategy/test.py
+++ strategy/test.py
@@ -1,7 +1,7 @@
 import logging
 
 from vnpy_ctastrategy import *
-from vnpy.trader.constant import Interval
+from vnpy.trader.constant import Interval, Offset
 from ctp.output import to_string
 
 from ctp.settings import SETTINGS
@@ -10,6 +10,8 @@ class MyTestStrategy(CtaTemplate):
     """测试策略"""
 
     _logger: logging.Logger = None
+    bought: bool = False
+    sold: bool = False
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
         """初始化策略"""
@@ -20,7 +22,7 @@ class MyTestStrategy(CtaTemplate):
         # 创建K线合成器
         self.bg = BarGenerator(on_bar=self.on_bar, window=1, on_window_bar=None, interval=Interval.MINUTE, daily_end=None)
         # 创建时间序列管理器
-        self.am = ArrayManager(size=5)
+        self.am = ArrayManager(size=3)
         self._logger.info(f"{self.strategy_name} __init__()")
 
     def on_init(self):
@@ -44,30 +46,32 @@ class MyTestStrategy(CtaTemplate):
 
     def on_bar(self, bar: BarData):
         """K线数据更新回调"""
-        self._logger.info(f"{self.strategy_name} on_bar(): {to_string(bar)}")
         self.am.update_bar(bar)
         if not self.am.inited:
-            self._logger.info(f"{self.strategy_name} initializing: count={self.am.count}, size={self.am.size}")
+            self._logger.info(f"{self.strategy_name} initializing: {self.am.count}/{self.am.size}")
             return
 
-        if self.pos == 0:
+        if not self.bought:
+            self._logger.info(f"{self.strategy_name} 正在买入")
             self.buy(bar.close_price, 1)
-        elif self.pos == 1:
+        elif not self.sold and self.pos > 0:
+            self._logger.info(f"{self.strategy_name} 正在卖出")
             self.sell(bar.close_price, 1)
-        else:
-            self._logger.error(f"unexpected position: {self.pos}")
-
-        self.put_event()
 
     def on_order(self, order: OrderData):
         """委托更新回调"""
-        self._logger.info(f"{self.strategy_name} on_order()", to_string(order))
+        self._logger.info(f"{self.strategy_name} on_order(): {to_string(order)}")
 
     def on_trade(self, trade: TradeData):
         """成交更新回调"""
-        self._logger.info(f"{self.strategy_name} on_trade()", to_string(trade))
-        self.put_event()
-
+        if not self.bought and trade.direction == Direction.LONG and trade.volume == 1:
+            self.bought = True
+            self._logger.info(f"{self.strategy_name} 买入测试成功! {to_string(trade)}")
+        elif not self.sold and trade.direction == Direction.SHORT and trade.volume == 1:
+            self.sold = True
+            self._logger.info(f"{self.strategy_name} 卖出测试成功! {to_string(trade)}")
+        else:
+            self._logger.warning(f"{self.strategy_name} unexpected on_trade(): {to_string(trade)}")
     def on_stop_order(self, stop_order: StopOrder):
         """停止单更新回调"""
-        self._logger.info(f"{self.strategy_name} on_stop_order()", to_string(stop_order))
+        self._logger.info(f"{self.strategy_name} on_stop_order(): {to_string(stop_order)}")
